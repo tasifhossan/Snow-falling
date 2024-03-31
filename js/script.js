b5f8c7123ecc63ba1dccfcb727bbd09dcceebac0
Mohammad Tasif Hossan <153765460+tasifhossan@users.noreply.github.com>
2024-03-31 19:41:19 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// js
+
+
+// jq
+$(document).ready(function(){
+
+$('body').flurry({
+  character: ["❆", "❅"],
+  height: 640,
+  speed: 3000,
+  wind: 100,
+  windVariance: 220,
+  frequency: 10,
+  large: 40,
+  small: 4
+});
+
+
+
+});
